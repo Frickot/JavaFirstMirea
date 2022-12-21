e7c7ec228983df1793ac2907da611cbf464458e9
Vadim Kabanov <113011326+Frickot@users.noreply.github.com>
2022-12-22 00:58:44 +0300

    Delete DivisionByZeroException.java

--- src/ru/mirea/task22/DivisionByZeroException.java
+++
@@ -1,8 +0,0 @@
-package ru.mirea.task22;
-
-public class DivisionByZeroException extends Exception {
-
-    public DivisionByZeroException(){
-        super("Division by zero! Retry with another expression");
-    }
-}
